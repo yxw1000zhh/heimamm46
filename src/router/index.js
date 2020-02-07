c7bf478bb06c4346a99ddb4be4f42a25486f16c6
zhh <you@example.com>
2020-02-07 12:47:29 +0800

    '整合路由'

---
+++ src/router/index.js
@@ -0,0 +1,12 @@
+import Vue from 'vue'
+import VueRouter from 'vue-router'
+//注册 vue-router
+Vue.use(VueRouter)
+// 实例化
+const router = new VueRouter({
+//这里就是路由的配制项
+routes: [
+
+  ]
+})
+export default router
